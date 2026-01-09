2ca534a9f87fd3f33915a9c6ee114aa2ca458343
mohamed-anis-chehili <mohamed.anis.chehili@ensia.edu.dz>
2026-01-09 19:38:46 +0100

    Refactor settings.py to simplify configuration
    Removed environment-based DEBUG setting and database URL handling.

--- eventify_backend/Eventify/settings.py
+++ eventify_backend/Eventify/settings.py
@@ -11,19 +11,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from decouple import config
-import dj_database_url
-from environ import Env
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-env = Env()
-ENVIRONMENT = config('ENVIRONMENT')
-if ENVIRONMENT == 'development':
-  DEBUG = True
-else:
-  DEBUG = False
+
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,11 +114,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-POSTGRES_LOCALLY = True
-database_url = env('DATABASE_URL', default='')
-if database_url and (ENVIRONMENT == "production" or POSTGRES_LOCALLY == True):
-  DATABASES['default'] = dj_database_url.parse(database_url)
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
@@ -148,6 +137,14 @@ USE_TZ = True
 STATIC_URL = "static/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+'/media/'
 
 
 # Default primary key field type
