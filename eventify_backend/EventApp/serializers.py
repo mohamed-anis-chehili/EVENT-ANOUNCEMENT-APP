5179093e92e259fab2920da852d08bfe2e239662
mohamed-anis-chehili <mohamed.anis.chehili@ensia.edu.dz>
2026-01-10 09:52:18 +0100

    Refactor serializers for User, Photo, Comment, and Event

--- eventify_backend/EventApp/serializers.py
+++ eventify_backend/EventApp/serializers.py
@@ -1,46 +1,68 @@
+from rest_framework import serializers
+from .models import Event, User, Post, Comment, Photo, EventFavorite, Repost
+
+
 class UserSerializer(serializers.ModelSerializer):
     photo = serializers.SerializerMethodField()
-    
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'name', 'lastname', 'photo')
-    
-    def get_photo(self, obj):
-        if obj.photo:
-            # Return full URL for Cloudinary or local storage
-            if hasattr(obj.photo, 'url'):
-                url = obj.photo.url
-                # If it's a Cloudinary URL, it's already absolute
-                if url.startswith('http'):
-                    return url
-                # For local storage, build absolute URI
-                request = self.context.get('request')
-                if request:
-                    return request.build_absolute_uri(url)
-                return url
-        return None
+
+@@ -26,12 +22,9 @@ def get_photo(self, obj):
 
 
 class PhotoSerializer(serializers.ModelSerializer):
-@@ -16,11 +33,16 @@ class Meta:
+    image = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Photo
         fields = ['id', 'content_type', 'image', 'uploaded_at', 'event', 'post']
-
+    
     def get_image(self, obj):
         if obj.image:
             url = obj.image.url
-            # If it's a Cloudinary URL, it's already absolute
-            if url.startswith('http'):
-                return url
-            # For local storage, build absolute URI
-            request = self.context.get('request')
-            if request:
-                return request.build_absolute_uri(url)
-            return url
+@@ -46,51 +39,8 @@ def get_image(self, obj):
         return None
 
 class CommentSerializer(serializers.ModelSerializer):
-@@ -69,4 +91,3 @@ class Meta:
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+class PostSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    photos = PhotoSerializer(many=True, read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class EventSerializer(serializers.ModelSerializer):
+    creator = UserSerializer(read_only=True)
+    photos = PhotoSerializer(many=True, read_only=True)
+    posts = PostSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+
+class EventFavoriteSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    event = EventSerializer(read_only=True)
+
+    class Meta:
+        model = EventFavorite
+        fields = '__all__'
+
+
+class RepostSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    event = EventSerializer(read_only=True)
+
+    class Meta:
+        model = Repost
         fields = ['id', 'user', 'event', 'caption', 'created_at']
-
-
 
