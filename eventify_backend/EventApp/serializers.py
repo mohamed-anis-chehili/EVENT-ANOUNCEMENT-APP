6d8c9ab806e839d64f437dcb464861b2534bc9ee
mohamed-anis-chehili <mohamed.anis.chehili@ensia.edu.dz>
2026-01-10 09:46:55 +0100

    Refactor serializers by removing unused classes
    Removed unused serializer classes and fields for cleaner code.

--- eventify_backend/EventApp/serializers.py
+++ eventify_backend/EventApp/serializers.py
@@ -1,7 +1,3 @@
-from rest_framework import serializers
-from .models import Event, User, Post, Comment, Photo, EventFavorite, Repost
-
-
 class UserSerializer(serializers.ModelSerializer):
     photo = serializers.SerializerMethodField()
     
@@ -26,12 +22,9 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class PhotoSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
-    
-    class Meta:
-        model = Photo
+@@ -16,11 +33,16 @@ class Meta:
         fields = ['id', 'content_type', 'image', 'uploaded_at', 'event', 'post']
-    
+
     def get_image(self, obj):
         if obj.image:
             url = obj.image.url
@@ -46,51 +39,8 @@ class PhotoSerializer(serializers.ModelSerializer):
         return None
 
 class CommentSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-
-    class Meta:
-        model = Comment
-        fields = '__all__'
-
-
-class PostSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-    photos = PhotoSerializer(many=True, read_only=True)
-    comments = CommentSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Post
-        fields = '__all__'
-
-
-class EventSerializer(serializers.ModelSerializer):
-    creator = UserSerializer(read_only=True)
-    photos = PhotoSerializer(many=True, read_only=True)
-    posts = PostSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Event
-        fields = '__all__'
-
-
-class EventFavoriteSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-    event = EventSerializer(read_only=True)
-
-    class Meta:
-        model = EventFavorite
-        fields = '__all__'
-
-
-class RepostSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-    event = EventSerializer(read_only=True)
-
-    class Meta:
-        model = Repost
+@@ -69,4 +91,3 @@ class Meta:
         fields = ['id', 'user', 'event', 'caption', 'created_at']
-
-
 
 
 
